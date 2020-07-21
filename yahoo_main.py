7acba7892287d1c5a17d0acb5d68814809cbb8d6
Jordan M. Young <54070169+Jordan-M-Young@users.noreply.github.com>
2020-07-21 19:55:22 -0400

    Update yahoo_main.py

--- yahoo_main.py
+++ yahoo_main.py
@@ -9,37 +9,48 @@ import stock_download_yahoo as sdy
 
 
 """example of how the stock_download_yahoo functions can be combined to produce
-a working database miner for Y@h00 stock database"""
+a working database download manager for Y@h00 stock database"""
 
 
 
 #directory where you want your files to go to
-directory = 'C:/Users/jmyou/Desktop/Yahoo_DB_2020'
+directory = 'C:/Users/jmyou/Desktop/Yahoo_DB_2020_Weekly'
 
 #where your files are normally downloaded to
 download_dir = 'C:/Users/jmyou/Downloads'
 
+#Where your 'bad ticker file' should go
+bad_ticker_file = 'E:/PythonProjects/Yahoo_DataBase/Yahoo_Bad_Tickers_Weekly.csv'
 
+#directory of your driver
+driverpath = 'E:/PythonProjects/Yahoo_DataBase/chromedriver.exe'
 
+#frequency of the data you want to download '"1d"' '"1wk"' or '"1mo"'
+frequency = '"1wk"'
 
-
-#opens stock database file
+#gets list of tickers from the companylist.csv file
+#any list of tickers can be used though
 tickers = sdy.open_list('companylist')
+
 
 
 count = 0
 downloads = []
 failed_downloads = []
 Flag = False
-previous_downloads = sdy.check_downloads_yahoo(tickers,directory)
+previous_downloads = sdy.Check_Downloads_Yahoo(tickers,directory)
+bad_tickers = sdy.get_bad_tickers(bad_ticker_file)
 
 for i in range(len(tickers)):
     if tickers[i] in previous_downloads:
         count += 1
+    elif tickers[i] in bad_tickers:
+        count += 1
     else:
         try:
             #change frequency to 1mo for monthly data and 1wk for weekly data
-            sdy.stock_download_yahoo(tickers[i],directory,download_dir,Flag,frequency='"1d"')
+            sdy.Stock_Download_Yahoo(tickers[i],directory,download_dir,frequency,
+                                     bad_ticker_file,driverpath,Flag)
             if Flag == False:
                 count += 1
                 downloads.append(tickers[i])
