34124e9e859058fc857ac300d08856058271ac92
Jordan M. Young <54070169+Jordan-M-Young@users.noreply.github.com>
2020-07-21 19:57:26 -0400

    Update stock_download_yahoo.py

--- stock_download_yahoo.py
+++ stock_download_yahoo.py
@@ -6,11 +6,14 @@ Created on Wed Feb 27 18:33:56 2019
 """
 
 import time
+import pandas as pd
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, NoSuchElementException
 import shutil
 import csv
 import os
+
+
 
 def get_bad_tickers(path):
     if os.path.exists(path):
@@ -21,7 +24,21 @@ def get_bad_tickers(path):
     
     return bad_tickers
 
-def check_downloads_yahoo(tickers,directory):
+
+def update_bad_tickers(path,ticker):
+    if  os.path.exists(path):
+        bad_tickers = pd.read_csv(path)
+        bad_tickers = list(bad_tickers.iloc[:,1])
+        bad_tickers.append(ticker)
+        bad_tickers = pd.DataFrame(bad_tickers)
+        bad_tickers.to_csv(path)
+    
+    else:
+        bad_tickers = [ticker]
+        bad_tickers = pd.DataFrame(bad_tickers)
+        bad_tickers.to_csv(path)
+
+def Check_Downloads_Yahoo(tickers,directory):
     """checks working directory for downloaded stock files
     prevents going over per day limit"""
     files = []
@@ -75,57 +92,71 @@ def move_file(ticker,directory,download_dir):
     shutil.move(old_name,new_path)
 
 def locate_click(driver,elem_path,Flag):
+    
     """locates and clicks an element in the xml doc based on its xpath"""
+    Flag = Flag
     try:
         x = driver.find_element_by_xpath(elem_path)
         x.click()
-    except WebDriverException:
+    except NoSuchElementException:
         try:
+            time.sleep(2)
             x = driver.find_element_by_xpath(elem_path)
             x.click()
-        except WebDriverException:
-            Flag = True
-            return Flag
+        except NoSuchElementException:
+            time.sleep(3)
+            x = driver.find_element_by_xpath(elem_path)
+            x.click()
         
-def stock_download_yahoo(ticker,directory,download_dir,Flag,frequency='"1d"',
-                         driverpath='E:/PythonProjects/Yahoo_DataBase/chromedriver.exe'):
-    """downloads stock data csv file for the selected ticker"""
-    driver = webdriver.Chrome(driverpath)
-    url = 'https://finance.yahoo.com/quote/' + ticker + '/history?p=' + ticker
-    driver.get(url)
+def Stock_Download_Yahoo(ticker,directory,download_dir,frequency,
+                         bad_ticker_file,driverpath,Flag):
+    try:
+        """downloads stock data csv file for the selected ticker"""
+        driver = webdriver.Chrome(driverpath)
+        url = 'https://finance.yahoo.com/quote/' + ticker + '/history?p=' + ticker
+        driver.get(url)
+        
+        #Xpath of each element that requires an interaction to get the correct
+        #downloaded file
+        date_button_path = '//div[@class="Pos(r) D(ib) Va(m) Mstart(8px)"]'
+        max_button_path = '//button[@data-value="MAX"]' 
+        # done_button_path = '//button[@class=" Bgc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) C(white) Bgc($linkActiveColor):h Bd(0) D(ib) Cur(p) Td(n)  Py(9px) Miw(80px)! Fl(start)"]'
+        frequency_button_path = '//span[@data-test="historicalFrequency-selected"]'
+        #for time period, 1mo = month , 1d = day, 1wk = week
+        frequency_menu_path = '//div[@data-value=' + frequency + ']'
+        apply_button_path = '//button[@class=" Bgc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) C(white) Bgc($linkActiveColor):h Bd(0) D(ib) Cur(p) Td(n)  Py(9px) Fl(end)"]'
+        download_button_path = '//a[@download="' + ticker + '.csv"]'
+        
+        #locates and clicks each required element to facilitate a correct download
+        time.sleep(5)
+        driver.execute_script("window.scrollTo(0,200)")
+        locate_click(driver,date_button_path,Flag)
+        locate_click(driver,max_button_path,Flag)
+        driver.execute_script("window.scrollTo(200,300)")
+        # print('done')
+        # # locate_click(driver,done_button_path,Flag)
+        locate_click(driver,frequency_button_path,Flag)
+        locate_click(driver,frequency_menu_path,Flag)
+        locate_click(driver,apply_button_path,Flag)
+        
+        #waits twenty seconds before downloading file; allows web app to update
+        #data
+        time.sleep(5)
+        #locates and downloads spreadsheet
+        locate_click(driver,download_button_path,Flag)
+        time.sleep(2)
+        
+        move_file(ticker, directory, download_dir)
+        #closes the browser
+        print(ticker + ': success')
+        time.sleep(5)
+        driver.quit()
     
-    #Xpath of each element that requires an interaction to get the correct
-    #downloaded file
-    date_button_path = '//div[@class="Pos(r) D(ib) Va(m) Mstart(8px)"]'
-    max_button_path = '//button[@data-value="MAX"]' 
-    done_button_path = '//button[@class=" Bgc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) C(white) Bgc($linkActiveColor):h Bd(0) D(ib) Cur(p) Td(n)  Py(9px) Miw(80px)! Fl(start)"]'
-    frequency_button_path = '//span[@data-test="historicalFrequency-selected"]'
-    #for time period, 1mo = month , 1d = day, 1wk = week
-    frequency_menu_path = '//div[@data-value=' + frequency + ']'
-    apply_button_path = '//button[@class=" Bgc($linkColor) Bdrs(3px) Px(20px) Miw(100px) Whs(nw) Fz(s) Fw(500) C(white) Bgc($linkActiveColor):h Bd(0) D(ib) Cur(p) Td(n)  Py(9px) Fl(end)"]'
-    download_button_path = '//a[@download="' + ticker + '.csv"]'
-    
-    #locates and clicks each required element to facilitate a correct download
-    time.sleep(5)
-    driver.execute_script("window.scrollTo(0,200)")
-    locate_click(driver,date_button_path,Flag)
-    locate_click(driver,max_button_path,Flag)
-    driver.execute_script("window.scrollTo(200,300)")
-    locate_click(driver,done_button_path,Flag)
-    locate_click(driver,frequency_button_path,Flag)
-    locate_click(driver,frequency_menu_path,Flag)
-    locate_click(driver,apply_button_path,Flag)
-    
-    #waits twenty seconds before downloading file; allows web app to update
-    #data
-    time.sleep(5)
-    #locates and downloads spreadsheet
-    locate_click(driver,download_button_path,Flag)
-    time.sleep(2)
-    
-    move_file(ticker, directory, download_dir)
-    #closes the browser
-    time.sleep(5)
-    driver.quit()
+    except NoSuchElementException:
+        update_bad_tickers(bad_ticker_file,ticker)
+        driver.quit()
+   
+        
     return Flag
+
 
